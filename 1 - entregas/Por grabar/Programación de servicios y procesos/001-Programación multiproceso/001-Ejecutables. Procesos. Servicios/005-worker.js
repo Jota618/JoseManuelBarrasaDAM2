e69781b09814de1bdbe1eced4832aa2cf13588fe
Jota618 <barrasa618@gmail.com>
2024-11-13 21:15:25 +0100

    servicios otra vez

--- 1 - entregas/Por grabar/Programación de servicios y procesos/001-Programación multiproceso/001-Ejecutables. Procesos. Servicios/005-worker.js
+++
@@ -1,4 +0,0 @@
-onmessage = function(){                                 // El worker escucha
-    console.log("Funciono");                             // Hace algo
-    postMessage("Hola soy el worker y vuelvo al proceso principal")       // Devuelve un mensaje al hilo principal
-}
